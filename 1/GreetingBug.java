c10fe84e060b08e598285a2711f4654f21350de4
miyazato1872 <miyazato5997@gmail.com>
2023-03-13 23:20:54 +0900

    演習問題
    https://www.youtube.com/watch?v=BBKVCkdqS8g&t=0s

---
+++ 1/GreetingBug.java
@@ -0,0 +1,11 @@
+public class GreetingBug {
+    public static void main(String[] args) {
+        System.out.println("こんにちは。");
+        System.out.println("一緒にがんばりましょう！");
+    }
+}
+
+//String[]args
+// System,のカンマ
+//最後の}の向き
+
