4af3a3550a467c9e0731d2661da658cf2f6a06b9
miyazato1872 <miyazato5997@gmail.com>
2023-03-14 00:18:28 +0900

    演算子
    https://www.youtube.com/watch?v=BBKVCkdqS8g&t=0s

---
+++ 1/Operator.java
@@ -0,0 +1,17 @@
+public class Operator {
+    public static void main(String[] args){
+        // コマンドラインから入力したデータ（文字列型）を
+        // int型に変換する
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+
+        System.out.println("a = " + a + ", b = " + b);
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b)); //int型なので、小数点以下は切り捨て
+        System.out.println("a % b = " + (a % b));
+        System.out.println("a >= 0：" + (a >= 0));
+        System.out.println("(a >= 0) && (b >= 0)：" + ((a >= 0) && (b >= 0)));
+    }
+}
