9a1ab4dce99df09a86c9514d4f990117184e4dc6
miyazato1872 <miyazato5997@gmail.com>
2023-03-30 22:54:56 +0900

    Cランクスキルチェック
    https://www.youtube.com/watch?v=aecDJ0yN1oY&list=PLru8-xuz0YvwoVRzBTX5jUcavM4Q8gZi9&index=7

--- 4/Main.java
+++ 4/Main.java
@@ -3,24 +3,65 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
         sc.nextLine();
 
-        int[] num = new int[n];
+        String cards; //4桁の番号を入れる入れる変数
 
-        for (int i = 0; i < n ; i++) {
-            num[i] = sc.nextInt();
+        for (int i  = 0; i < n; i++) {
+            cards = sc.nextLine();  //1回目でcards[0]に、"7777"を代入
+            String[] num = cards.split("");
+            Arrays.sort(num);
+            Judgment(num);
         }
-        Arrays.sort(num);
-
-        int[] descending = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            descending[i] = num[n - i - 1];
-        }
-
-        for (int x : descending) {
-            System.out.println(x);
+    }
+    public static void Judgment(String num[]) {
+        if (Objects.equals(num[0], num[1]) && Objects.equals(num[1], num[2]) && Objects.equals(num[2], num[3])) {
+            System.out.println("Four Card");
+        } else if (Objects.equals(num[0], num[1]) && Objects.equals(num[1], num[2])) {
+            System.out.println("Three Card");
+        } else if (Objects.equals(num[1], num[2]) && Objects.equals(num[2], num[3])) {
+            System.out.println("Three Card");
+        } else if (Objects.equals(num[0], num[1]) && Objects.equals(num[2], num[3])) {
+            System.out.println("Two Pair");
+        } else if (Objects.equals(num[0], num[1]) || Objects.equals(num[1], num[2]) || Objects.equals(num[2], num[3])) {
+            System.out.println("One Pair");
+        } else {
+            System.out.println("No Pair");
         }
     }
 }
+
+
+//import java.util.*;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//
+//        int n = sc.nextInt();
+//        sc.nextLine();
+//
+//        String cards; //4桁の番号を入れる入れwつ
+//
+//        for (int i  = 0; i < n; i++) {
+//            cards = sc.nextLine();  //1回目でcards[0]に、"7777"を代入
+//            String[] num = cards.split("");
+//            Arrays.sort(num);
+//            if (Objects.equals(num[0], num[1]) && Objects.equals(num[1], num[2]) && Objects.equals(num[2], num[3])) {
+//                System.out.println("Four Card");
+//            } else if (Objects.equals(num[0], num[1]) && Objects.equals(num[1], num[2])) {
+//                System.out.println("Three Card");
+//            } else if (Objects.equals(num[1], num[2]) && Objects.equals(num[2], num[3])) {
+//                System.out.println("Three Card");
+//            } else if (Objects.equals(num[0], num[1]) && Objects.equals(num[2], num[3])) {
+//                System.out.println("Two Pair");
+//            } else if (Objects.equals(num[0], num[1]) || Objects.equals(num[1], num[2]) || Objects.equals(num[2], num[3])) {
+//                System.out.println("One Pair");
+//            } else {
+//                System.out.println("No Pair");
+//            }
+//        }
+//    }
+//}
